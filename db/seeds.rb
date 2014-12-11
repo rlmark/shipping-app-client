cd288175016c3020650e0c7979ab77c1dc0a8ac1
Bookis Smuin <bookis.smuin@gmail.com>
2014-12-11 13:32:44 -0800

    add product views

---
+++ db/seeds.rb
@@ -0,0 +1,73 @@
+Product.destroy_all
+
+Product.create(
+  name: "Dinosaur",
+  description: "Dinosaurs are a diverse group of animals of the clade Dinosauria. They first appeared during the Triassic period, 231.4 million years ago, and were the dominant terrestrial vertebrates for 135 million years, from the beginning of the Jurassic (about 201 million years ago) until the end of the Cretaceous (66 million years ago), when the Cretaceous–Paleogene extinction event led to the extinction of most dinosaur groups at the close of the Mesozoic Era. The fossil record indicates that birds evolved from theropod dinosaurs during the Jurassic Period and, consequently, they are considered a subgroup of dinosaurs by many paleontologists.",
+  weight: 12.0,
+  price_cents: 3500,
+  remote_image_url: "http://ecx.images-amazon.com/images/I/71NUl-pWrsL._AA1500_.jpg"
+)
+
+Product.create(
+  name: "Slinky",
+  description: "Slinky is a toy, a precompressed helical spring invented by Richard James in the early 1940s. It can perform a number of tricks, including travelling down a flight of steps end-over-end as it stretches and re-forms itself with the aid of gravity and its own momentum, or appear to levitate for a period of time after it has been dropped. These interesting characteristics have contributed to its success as a toy in its home country of USA, resulting in many popular toys with slinky components in a wide range of countries.",
+  weight: 1.0,
+  price_cents: 1000,
+  remote_image_url: "http://c4.yoyo.com/images/products/p/fo/fo-002_1z.jpg"
+)
+
+Product.create(
+  name: "Etch-A-Sketch",
+  description: "An Etch A Sketch has a thick, flat gray screen in a red plastic frame. There are two knobs on the front of the frame in the lower corners. Twisting the knobs moves a stylus that displaces aluminum powder on the back of the screen, leaving a solid line. The knobs create lineographic images. The left control moves the stylus horizontally, and the right one moves it vertically.",
+  weight: 6,
+  price_cents: 2000,
+  remote_image_url: "http://c4.yoyo.com/images/products/p/zeh/zeh-033_1z.jpg"
+)
+
+Product.create(
+  name: "Mrs. Patato Head",
+  description: "Mrs. Potato Head is an American toy consisting of a plastic model of a potato, which can be decorated with a variety of plastic parts that can attach to the main body. These parts usually include ears, eyes, shoes, a hat, a nose, and a mouth. ",
+  weight: 4.5,
+  price_cents: 1299,
+  remote_image_url: "http://timewellness.files.wordpress.com/2011/02/active-adventures-mrs-potato-head.jpg"
+)
+
+Product.create(
+  name: "Army Men",
+  description: "Unlike the more expensive toy soldiers available in hobby shops, army men are sold at low prices in discount stores, supermarkets, and dollar stores. Also unlike many toy soldiers, army men are sold unpainted and almost always dressed in modern military uniforms and armed with 20th-century weapons.",
+  weight: 0.2,
+  price_cents: 99,
+  remote_image_url: "http://c3.yoyo.com/images/products/p/qj/qj-997_1z.jpg"
+)
+
+Product.create(
+  name: "Inflatable Astronaut",
+  description: "An astronaut or cosmonaut is a person trained by a human spaceflight program to command, pilot, or serve as a crew member of a spacecraft. Although generally reserved for professional space travelers, the terms are sometimes applied to anyone who travels into space, including scientists, politicians, journalists, and tourists.",
+  weight: 0.5,
+  price_cents: 45.95,
+  remote_image_url: "http://www.partykitnkaboodle.com.au/images/inflatable_astronaut.jpg"
+)
+
+Product.create(
+  name: "Plush Lego Man",
+  description: "Lego[a] (/ˈlɛɡoʊ/[2]) is a popular line of construction toys manufactured by The Lego Group, a privately held company based in Billund, Denmark. The company's flagship product, Lego, consists of colourful interlocking plastic bricks and an accompanying array of gears, minifigures and various other parts. Lego bricks can be assembled and connected in many ways, to construct such objects as vehicles, buildings, and even working robots. Anything constructed can then be taken apart again, and the pieces used to make other objects.",
+  weight: 12,
+  price_cents: 2500,
+  remote_image_url: "http://www.tystoybox.com/media/catalog/product/LGO-1040-LPM-BLUE.jpg"
+)
+
+Product.create(
+  name: "Race Car",
+  description: "Auto racing (also known as car racing, motor racing[1] or automobile racing) is a sport involving the racing of automobiles for competition. The main aim of an individual event is to set the fastest time in a set number of laps or time limit. The finishing order is determined by race time, with the fastest time in first place, second-fastest in second place and so on.",
+  weight: 3.5,
+  price_cents: 1299,
+  remote_image_url: "http://assets.blog.hemmings.com/wp-content/uploads//2014/01/Agajanian-Special.png"
+)
+
+Product.create(
+  name: "Robot",
+  description: "A robot is an automatic mechanical device often resembling a human or animal. Modern robots are usually an electro-mechanical machine guided by a computer program or electronic circuitry.",
+  weight: 45.5,
+  price_cents: 100095,
+  remote_image_url: "http://www.freakingnews.com/images/app_images/old-robot.jpg"
+)
