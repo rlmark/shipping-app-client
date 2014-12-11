96b07a51a8d4f2c3182f2e1b65d8038d443a1d31
Bookis Smuin <bookis.smuin@gmail.com>
2014-12-11 10:20:40 -0800

    add product controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get "/products",                 to: "products#index", as: :products
+  get "/products/:canonical_name", to: "products#show",  as: :product
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
