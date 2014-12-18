1a984a4bac7ef0de128d029be1fb4cbac3b99334
Rebecca Mark <rebecca.lmark@gmail.com>
2014-12-18 11:48:25 -0800

    displaying hard coded data from api

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,5 @@
 class CartController < ApplicationController
+  include HTTParty
 
   def create
     if current_order.items.create(product_id: params[:product_id])
@@ -6,6 +7,9 @@ class CartController < ApplicationController
     end
   end
 
+  # This is where the address gets displayed, and where the shipping option gets displayed.
   def show
+    # do a get request to my api and set = to instance variable.
+    @response = HTTParty.get('https://ship-toys-api.herokuapp.com/quotes/calculate?provider=fedex&destination_country=US&destination_state=WA&destination_city=Seattle&destination_zip=98105&package_weight=14')
   end
 end
